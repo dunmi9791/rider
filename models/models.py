dc7f67d0722cf2f488b9a54a253963e10c2fc98c
dunmi9791 <dominic.anyanna@gmail.com>
2019-05-22 11:26:25 +0100

    first menus

--- models/models.py
+++ models/models.py
@@ -2,6 +2,33 @@
 
 from odoo import models, fields, api
 
+class ServiceRequest(models.Model):
+    _name = 'servicerequest.rider'
+
+    vehicle_id = fields.Many2one('vehicle')
+    checkin_date = fields.Datetime(string="Check-in Date/Time", required=False, )
+    checkout_date = fields.Datetime(string="Check-out Date/Time", required=False, )
+    electrics_ta = fields.Selection(string="Electronic Assessment", selection=[('1', '1'), ('2', '2'),('3', '3'),('4', '4'), ('5', '5'), ], required=False, )
+    suspension_ta = fields.Selection(string="Suspension Assessment",
+                                     selection=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ],
+                                     required=False, )
+    engine_ta = fields.Selection(string="Engine Assessment",
+                                     selection=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ],
+                                     required=False, )
+    bodywork_ta = fields.Selection(string="Body Work Assessment",
+                                     selection=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ],
+                                     required=False, )
+    interior_ta = fields.Selection(string="Interior Assessment",
+                                     selection=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ],
+                                     required=False, )
+    tyres_ta = fields.Selection(string="Tyres Assessment",
+                                     selection=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ],
+                                     required=False, )
+    service_type = fields.Selection(string="Serivice Type", selection=[('Planned', 'Planned'), ('Accident', 'Accident'),('Breakdown', 'Breakdown'), ('Technical Problem', 'Technical Problem'), ('Demand Service','Demand service'), ('Return Job', 'Return Job'), ], required=False, )
+    checkin_comment = fields.Text(string="Check-in Comment", required=False, )
+    technical_comment = fields.Text(string="Description of work required identified br technical assessment", required=False, )
+    
+
 # class rider(models.Model):
 #     _name = 'rider.rider'
 
