6f7539b81d81ba49016c74fda4dd1504b88bba27
dunmi9791 <dominic.anyanna@gmail.com>
2019-05-22 12:57:45 +0100

    first menus

--- models/models.py
+++ models/models.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 class ServiceRequest(models.Model):
     _name = 'servicerequest.rider'
 
-    vehicle_id = fields.Many2one('vehicle')
+    vehicle_id = fields.Many2one('vehicles.rider')
     checkin_date = fields.Datetime(string="Check-in Date/Time", required=False, )
     checkout_date = fields.Datetime(string="Check-out Date/Time", required=False, )
     electrics_ta = fields.Selection(string="Electronic Assessment", selection=[('1', '1'), ('2', '2'),('3', '3'),('4', '4'), ('5', '5'), ], required=False, )
@@ -27,6 +27,26 @@ class ServiceRequest(models.Model):
     service_type = fields.Selection(string="Serivice Type", selection=[('Planned', 'Planned'), ('Accident', 'Accident'),('Breakdown', 'Breakdown'), ('Technical Problem', 'Technical Problem'), ('Demand Service','Demand service'), ('Return Job', 'Return Job'), ], required=False, )
     checkin_comment = fields.Text(string="Check-in Comment", required=False, )
     technical_comment = fields.Text(string="Description of work required identified br technical assessment", required=False, )
+    spare_tyre = fields.Boolean(string="Spare Tyre",  )
+    jack = fields.Boolean(string="Jack",  )
+    wheel_spanner = fields.Boolean(string="Wheel Spanner",  )
+    tools = fields.Boolean(string="Tools",  )
+    caution_triangle = fields.Boolean(string="Caution Triangle",  )
+    fire_extinguisher = fields.Boolean(string="Fire Extinguisher",  )
+    new_field_ids = fields.Many2many(comodel_name="", relation="", column1="", column2="", string="", )
+
+
+
+
+class Vehicle(models.Model):
+    _name = 'vehicles.rider'
+    _rec_name = 'vehicles'
+    _description = 'Vehicles'
+
+    vehicle_type = fields.Char()
+    vehicle_registration = fields.Char()
+    client_id = fields.Many2one(comodel_name="client.riders", string="Client", required=False, )
+
     
 
 # class rider(models.Model):
