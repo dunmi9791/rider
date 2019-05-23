f00d1afb1391840c6923de50e5edee21effb6937
dunmi9791 <dominic.anyanna@gmail.com>
2019-05-23 22:56:16 +0100

    order memo

--- models/ordermemo.py
+++ models/ordermemo.py
@@ -8,8 +8,7 @@ class OrderMemo(models.Model):
     _description = 'Order Memo'
 
     subject = fields.Char(string="Subject")
-    employee_from_id = fields.Many2one(comodel_name="res.users", string="From", required=False, )
-    employee_to_id = fields.Many2one(comodel_name="res.users", string="To", required=False, )
+
     date = fields.Date(string="Date", required=False, )
     memo_content = fields.HTML(string="",  )
 
