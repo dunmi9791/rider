fb95ff62b2c670d8343932fdf62308d6e8e14cd7
dunmi9791 <dominic.anyanna@gmail.com>
2019-05-23 22:47:58 +0100

    order memo

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,5 @@ from . import models
 from . import vehicles
 from . import cash_requisition
 from . import orders
+from . import partner
+from . import ordermemo
---
+++ models/ordermemo.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class OrderMemo(models.Model):
+    _name = 'ordermemo.rider'
+    _rec_name = 'subject'
+    _description = 'Order Memo'
+
+    subject = fields.Char(string="Subject")
+    employee_from_id = fields.Many2one(comodel_name="res.users", string="From", required=False, )
+    employee_to_id = fields.Many2one(comodel_name="res.users", string="To", required=False, )
+    employee_copy_ids = fields.Many2many(comodel_name="res.users", relation="", column1="", column2="", string="CC", )
+    date = fields.Date(string="Date", required=False, )
+    memo_content = fields.HTML(string="",  )
+
+
