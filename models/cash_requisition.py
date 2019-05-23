838a3359587d0cacfbbe90af7e390ae0ce465063
dunmi9791 <dominic.anyanna@gmail.com>
2019-05-23 17:45:55 +0100

    Cash requisition

---
+++ models/cash_requisition.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class CashRequisition(models.Model):
+    _name = 'cashrequisition.rider'
+    _description = 'cash requisition'
+
+    date = fields.Date(string="Date", required=False, )
+    amount_words = fields.Char(string="Amount in Words", required=False, )
+    amount_figures = fields.Float(string="Amount in Figures",  required=False, )
+    payable = fields.Char(string="Payable to", required=False, )
+    details = fields.Text(string="Details", required=False, )
+    state = fields.Selection(string="", selection=[('Requested', 'Requested'), ('Authorised', 'Authorised'), ('Approved', 'Approved'), ('Processed', 'Processed'), ('Received', 'Received'), ], required=False, )
+
+
