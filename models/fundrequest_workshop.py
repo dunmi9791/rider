365481801a5937f63d572217c7327cc22bdc40cd
dunmi9791 <dominic.anyanna@gmail.com>
2019-05-27 10:16:46 +0100

    fundrequest

--- models/fundrequest_workshop.py
+++ models/fundrequest_workshop.py
@@ -12,7 +12,7 @@ class FundRequestWorkshop(models.Model):
     request_no = fields.Char(string="Request Number" , requires=False, )
     programme_id = fields.Many2one(comodel_name="programme", string="Programme ID", required=False, )
     jobcard_id = fields.Many2one(comodel_name="servicerequest.rider", string="Job Card ref", required=False, )
-    state = fields.Selection(string="", selection=[('draft', 'draft'), ('Requested', 'Requested'), ('Approved', 'Approved'), ], required=False, copy=False, default='draft', readonly=True, track_visibility='onchange', )
+    state = fields.Selection(string="", selection=[('draft', 'draft'), ('Requested', 'Requested'), ('Approved', 'Approved'), ('Rejected', 'Rejected',) ], required=False, copy=False, default='draft', readonly=True, track_visibility='onchange', )
     operations = fields.One2many(
         'fundrequest.partsline', 'fundrequest_id', 'Parts',
         copy=True, readonly=True, states={'draft': [('readonly', False)]})
@@ -25,6 +25,17 @@ class FundRequestWorkshop(models.Model):
 
         self.amount_total = sum(operation.price_subtotal for operation in self.operations)
 
+    @api.multi
+    def workshop_fund_request(self):
+        self.state = 'Requested'
+
+    @api.multi
+    def workshop_fund_approve(self):
+        self.state = 'Approved'
+
+    @api.multi
+    def workshop_fund_reject(self):
+        self.state = 'Rejected'
 
 class Parts(models.Model):
     _name = 'parts.rider'
