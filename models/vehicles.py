bca5422dad7607c68d1196226a85be74b3ec68e6
dunmi9791 <dominic.anyanna@gmail.com>
2019-06-04 15:17:21 +0100

    vehicle models add

--- models/vehicles.py
+++ models/vehicles.py
@@ -5,6 +5,33 @@ from odoo import models, fields, api
 class Vehicle(models.Model):
     _name = 'vehicles.rider'
 
-    vehicle_type = fields.Char()
+    vehicle_type = fields.Many2one(comodel_name="vehichletype.rider", string="Vehicle Type")
+    vehicle_make = fields.Many2one(comodel_name="vehichlemake.rider", string="Vehicle Make")
+    vehicle_model = fields.Many2one(comodel_name="vehichlemodel.rider", string="Vehicle Model")
     vehicle_registration = fields.Char()
     client_id = fields.Many2one(comodel_name="res.partner", string="Client", required=False, )
+
+
+
+class VehicleType(models.Model):
+    _name = 'vehicletype.rider'
+    _rec_name = 'name'
+    _description = 'Vehicle Type'
+
+    name = fields.Char()
+
+class VehicleMake(models.Model):
+    _name = 'vehiclemake.rider'
+    _rec_name = 'name'
+    _description = 'Vehicle Make'
+
+    name = fields.Char()
+
+
+class VehicleModel(models.Model):
+    _name = 'vehiclemodel.rider'
+    _rec_name = 'name'
+    _description = 'Vehicle Model'
+
+    name = fields.Char()
+
