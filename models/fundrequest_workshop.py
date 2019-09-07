aeee0da44a3527011373373068218c49298894cc
dunmi9791 <dominic.anyanna@gmail.com>
2019-09-07 06:36:14 +0100

    extentend conditon

--- models/fundrequest_workshop.py
+++ models/fundrequest_workshop.py
@@ -19,9 +19,10 @@ class FundRequestWorkshop(models.Model):
     state = fields.Selection(string="", selection=[('draft', 'draft'), ('Requested', 'Requested'), ('Approved', 'Approved'), ('Rejected', 'Rejected'),], required=False, copy=False, default='draft', readonly=True, track_visibility='onchange', )
     operations = fields.One2many(
         'fundrequest.partsline', 'fundrequest_id', 'Parts',
-        copy=True, readonly=True, states={'draft': [('readonly', False)]}, related='jobcard_id.operations')
+        copy=True, readonly=True, states={'draft': [('readonly', False)]})
     part_qty = fields.Float(string="Quantity",  required=False, )
     amount_total = fields.Float('Total', compute='_amount_total', store=True)
+
 
 
 
@@ -65,7 +66,7 @@ class FundrequestLine(models.Model):
     name = fields.Text(string='Description', required=False)
     fundrequest_id = fields.Many2one(comodel_name="fundrequestw.rider", index=True, ondelete='cascade')
     parts_id = fields.Many2one('product.product', string='Parts',
-                                 ondelete='restrict', index=True)
+                                 ondelete='restrict', index=True, related='fundrequest_id.jobcard_id.operations')
     state = fields.Selection([
         ('draft', 'Draft'),
         ('Requested', 'Requested'),
