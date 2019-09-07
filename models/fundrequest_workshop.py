a10a748c216c79435bbd66b5d35cb9e3b09dd801
dunmi9791 <dominic.anyanna@gmail.com>
2019-09-07 06:17:03 +0100

    extentend conditon

--- models/fundrequest_workshop.py
+++ models/fundrequest_workshop.py
@@ -19,9 +19,11 @@ class FundRequestWorkshop(models.Model):
     state = fields.Selection(string="", selection=[('draft', 'draft'), ('Requested', 'Requested'), ('Approved', 'Approved'), ('Rejected', 'Rejected'),], required=False, copy=False, default='draft', readonly=True, track_visibility='onchange', )
     operations = fields.One2many(
         'fundrequest.partsline', 'fundrequest_id', 'Parts',
-        copy=True, readonly=True, states={'draft': [('readonly', False)]})
+        copy=True, readonly=True, states={'draft': [('readonly', False)]}, related='jobcard_id.operations')
     part_qty = fields.Float(string="Quantity",  required=False, )
     amount_total = fields.Float('Total', compute='_amount_total', store=True)
+
+
 
     @api.one
     @api.depends('operations.price_subtotal',)
